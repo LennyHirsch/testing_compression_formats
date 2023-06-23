2de04e6a9d96040bed05c321710e5f26550563ad
LennyHirsch <lenny.hirsch@gmail.com>
2023-06-23 23:54:03 +0100

    Attempting to implement hdf5 creation and reading. WIP.

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,55 @@ use std::env;
 const SIZE: usize = 1024 * 1024 * 8;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 3 { panic!("Usage: flag filename\nflag: c (compress) or d (decompress)") }
-    let flag = args[1].as_str();
-    let filename = &args[2];
+    // let args: Vec<String> = env::args().collect();
+    // if args.len() != 3 { panic!("Usage: flag filename\nflag: c (compress) or d (decompress)") }
+    // let flag = args[1].as_str();
+    // let filename = &args[2];
+    let test_file = read_hdf5().unwrap();
+    let test_file = test_file.as_reader();
+    let test_arr = test_file.read_2d::<u8>().unwrap();
+    println!("{:#?}", test_arr);
 
-    let now = Instant::now();
+    // let now = Instant::now();
 
-    match flag {
-        "c" => {
-            match compress(filename) {
-                Ok(_) => println!("File compressed."),
-                Err(e) => println!("An error occured: {}", e),
-            }
-        },
-        "d" => {
-            match decompress(filename) {
-                Ok(_) => println!("File decompressed."),
-                Err(e) => println!("An error occured: {}", e),
-            }
-        },
-        &_ => panic!("Usage: flag filename\nflag: c (compress) or d (decompress)"),
-    }
+    // match flag {
+    //     "c" => {
+    //         match compress(filename) {
+    //             Ok(_) => println!("File compressed."),
+    //             Err(e) => println!("An error occured: {}", e),
+    //         }
+    //     },
+    //     "d" => {
+    //         match decompress(filename) {
+    //             Ok(_) => println!("File decompressed."),
+    //             Err(e) => println!("An error occured: {}", e),
+    //         }
+    //     },
+    //     &_ => panic!("Usage: flag filename\nflag: c (compress) or d (decompress)"),
+    // }
 
-    let elapsed = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed);
+    // let elapsed = now.elapsed();
+    // println!("Elapsed: {:.2?}", elapsed);
+}
+
+fn create_hdf5() -> hdf5::Result<()> {
+    let file = hdf5::File::create("testhdf.h5")?;
+    let testarr = ndarray::array![[1,2,3,4],[5,6,7,8]];
+    let anotherarr = ndarray::array![[10,20,30,40],[50,60,70,80]];
+    let colors = file.new_dataset::<u8>().create("testArray")?;
+    colors.write(&testarr)?;
+
+    let group = file.create_group("testGroup")?;
+    let pixels = group.new_dataset::<u8>().create("anotherArray")?;
+    pixels.write(&anotherarr)?;
+
+    Ok(())
+}
+
+fn read_hdf5() -> hdf5::Result<hdf5::Dataset> {
+    let file = hdf5::File::open("testhdf.h5")?;
+    let arr = file.dataset("testArray")?;
+    Ok(arr)
 }
 
 fn compress(input_file: &str) -> io::Result<()> {
