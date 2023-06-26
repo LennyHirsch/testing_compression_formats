48218cb359b2aa6c42863fe1b9a3540acefbce6e
LennyHirsch <lenny.hirsch@gmail.com>
2023-06-26 17:55:10 +0100

    Defined some types, still lots to go

---
+++ src/main copy.rs
@@ -0,0 +1,95 @@
+use zstd::stream::{copy_encode, copy_decode};
+use std::io::{self, BufReader, BufWriter, Write};
+use std::fs::File;
+use std::time::Instant;
+use std::path::Path;
+use std::env;
+
+const SIZE: usize = 1024 * 1024 * 8;
+
+fn main() {
+    // let args: Vec<String> = env::args().collect();
+    // if args.len() != 3 { panic!("Usage: flag filename\nflag: c (compress) or d (decompress)") }
+    // let flag = args[1].as_str();
+    // let filename = &args[2];
+    let test_file = create_hdf5();
+    // let test_file = read_hdf5().unwrap();
+    // let test_file = test_file.as_reader();
+    // let test_arr = test_file.read_2d::<u8>().unwrap();
+    // println!("{:#?}", test_arr);
+
+    // let now = Instant::now();
+
+    // match flag {
+    //     "c" => {
+    //         match compress(filename) {
+    //             Ok(_) => println!("File compressed."),
+    //             Err(e) => println!("An error occured: {}", e),
+    //         }
+    //     },
+    //     "d" => {
+    //         match decompress(filename) {
+    //             Ok(_) => println!("File decompressed."),
+    //             Err(e) => println!("An error occured: {}", e),
+    //         }
+    //     },
+    //     &_ => panic!("Usage: flag filename\nflag: c (compress) or d (decompress)"),
+    // }
+
+    // let elapsed = now.elapsed();
+    // println!("Elapsed: {:.2?}", elapsed);
+}
+
+fn create_hdf5() -> hdf5::Result<()> {
+    let file = hdf5::File::create("testhdf.h5")?;
+    let data = vec![1,2,3,4,5,6,7,8,9,0];
+    let data2 = ndarray::array![[1,2,3,4,5],[6,7,8,9,0]];
+    let group = file.create_group("example_group")?;
+    let data_set = group
+        .new_dataset::<i32>()
+        .shape([data.len()])
+        .create("example_Data")?;
+    data_set.write(&data)?;
+
+    Ok(())
+}
+
+fn read_hdf5() -> hdf5::Result<hdf5::Dataset> {
+    let file = hdf5::File::open("testhdf.h5")?;
+    let arr = file.dataset("testArray")?;
+    Ok(arr)
+}
+
+fn compress(input_file: &str) -> io::Result<()> {
+    let mut output_name = String::from(input_file.to_string());
+    output_name.push_str(".zstd");
+    let output_name = Path::new(&output_name);
+    
+    let input_file = File::open(input_file)?;
+    let output_file = File::create(output_name)?;
+
+    let mut reader = BufReader::with_capacity(SIZE, input_file);
+    let mut writer = BufWriter::new(output_file);
+
+    copy_encode(&mut reader, &mut writer, 0)?;
+
+    writer.flush()?;
+
+    Ok(())
+}
+
+fn decompress(input_file: &str) -> io::Result<()> {
+    let output_name = Path::new(input_file).file_stem().unwrap();
+    
+    let input_file = File::open(input_file)?;
+    let output_file = File::create(output_name)?;
+
+    let mut reader = BufReader::with_capacity(SIZE, input_file);
+    let mut writer = BufWriter::new(output_file);
+
+    copy_decode(&mut reader, &mut writer)?;
+
+    writer.flush()?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,77 @@
-use zstd::stream::{copy_encode, copy_decode};
-use std::io::{self, BufReader, BufWriter, Write};
-use std::fs::File;
-use std::time::Instant;
-use std::path::Path;
-use std::env;
+use hdf5::{File, H5Type, Result};
+use ndarray::{arr2, s};
 
-const SIZE: usize = 1024 * 1024 * 8;
-
-fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // if args.len() != 3 { panic!("Usage: flag filename\nflag: c (compress) or d (decompress)") }
-    // let flag = args[1].as_str();
-    // let filename = &args[2];
-    let test_file = create_hdf5();
-    // let test_file = read_hdf5().unwrap();
-    // let test_file = test_file.as_reader();
-    // let test_arr = test_file.read_2d::<u8>().unwrap();
-    // println!("{:#?}", test_arr);
-
-    // let now = Instant::now();
-
-    // match flag {
-    //     "c" => {
-    //         match compress(filename) {
-    //             Ok(_) => println!("File compressed."),
-    //             Err(e) => println!("An error occured: {}", e),
-    //         }
-    //     },
-    //     "d" => {
-    //         match decompress(filename) {
-    //             Ok(_) => println!("File decompressed."),
-    //             Err(e) => println!("An error occured: {}", e),
-    //         }
-    //     },
-    //     &_ => panic!("Usage: flag filename\nflag: c (compress) or d (decompress)"),
-    // }
-
-    // let elapsed = now.elapsed();
-    // println!("Elapsed: {:.2?}", elapsed);
+#[derive(H5Type, Clone, PartialEq, Debug)]
+#[repr(u8)]
+pub enum Colors {
+    R = 1,
+    G = 2,
+    B = 3,
 }
 
-fn create_hdf5() -> hdf5::Result<()> {
-    let file = hdf5::File::create("testhdf.h5")?;
-    let data = vec![1,2,3,4,5,6,7,8,9,0];
-    let data2 = ndarray::array![[1,2,3,4,5],[6,7,8,9,0]];
-    let group = file.create_group("example_group")?;
-    let data_set = group
-        .new_dataset::<i32>()
-        .shape([5,2])
-        .create("example_Data")?;
-    data_set.write(&data2)?;
+#[derive(H5Type, Clone, PartialEq, Debug)]
+#[repr(C)]
+pub struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+}
 
+#[derive(H5Type, Clone, PartialEq, Debug)]
+#[repr(C)]
+pub struct Pixel {
+    xy: (i64, i64),
+    color: Color,
+}
+
+impl Color {
+    pub fn new(r: u8, g: u8, b: u8) -> Self {
+        Self {r,g,b}
+    }
+}
+
+impl Pixel {
+    pub fn new(x: i64, y: i64, color: Color) -> Self {
+        Self { xy: (x,y), color}
+    }
+}
+
+fn write_hdf5() -> Result<()> {
+    use Colors::*;
+    let file = hdf5::File::create("pixels.h5")?;
+    let group = file.create_group("dir")?;
+    let builder = group.new_dataset_builder();
+    let ds = builder
+        .with_data(&arr2(&[
+            [Pixel::new(1,2, Color::new(1,2,3)), Pixel::new(2,3, Color::new(4,5,6))],
+            [Pixel::new(3,4,Color::new(7,8,9)), Pixel::new(4,5,Color::new(10,11,12))],
+            [Pixel::new(5,6,Color::new(13,14,15)), Pixel::new(6,7,Color::new(16,17,18))],
+        ]))
+        .create("pixels")?;
+    
+    let attr = ds.new_attr::<Colors>().shape([3]).create("colors")?;
+    attr.write(&[R,G,B])?;
     Ok(())
 }
 
-fn read_hdf5() -> hdf5::Result<hdf5::Dataset> {
-    let file = hdf5::File::open("testhdf.h5")?;
-    let arr = file.dataset("testArray")?;
-    Ok(arr)
-}
+// fn read_hdf5() -> Result<()> {
+//     use Color::*;
+//     let file = File::open("pixels.h5")?;
+//     let ds = file.dataset("dir/pixels")?;
+//     assert_eq!(
+//         ds.read_slice::<Pixel, _, _>(s![1.., ..])?,
+//         arr2(&[
+//             [Pixel::new(3,4,G), Pixel::new(4,5,R)],
+//             [Pixel::new(5,6,B), Pixel::new(6,7,G)],
+//         ])
+//     );
+//     let attr = ds.attr("colors")?;
+//     assert_eq!(attr.read_1d::<Color>()?.as_slice().unwrap(), &[R,G,B]);
+//     Ok(())
+// }
 
-fn compress(input_file: &str) -> io::Result<()> {
-    let mut output_name = String::from(input_file.to_string());
-    output_name.push_str(".zstd");
-    let output_name = Path::new(&output_name);
-    
-    let input_file = File::open(input_file)?;
-    let output_file = File::create(output_name)?;
-
-    let mut reader = BufReader::with_capacity(SIZE, input_file);
-    let mut writer = BufWriter::new(output_file);
-
-    copy_encode(&mut reader, &mut writer, 0)?;
-
-    writer.flush()?;
-
-    Ok(())
-}
-
-fn decompress(input_file: &str) -> io::Result<()> {
-    let output_name = Path::new(input_file).file_stem().unwrap();
-    
-    let input_file = File::open(input_file)?;
-    let output_file = File::create(output_name)?;
-
-    let mut reader = BufReader::with_capacity(SIZE, input_file);
-    let mut writer = BufWriter::new(output_file);
-
-    copy_decode(&mut reader, &mut writer)?;
-
-    writer.flush()?;
-
+fn main() -> Result<()> {
+    write_hdf5()?;
+    // read_hdf5()?;
     Ok(())
 }
